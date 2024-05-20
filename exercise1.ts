20696827087525d73aa19cfe9ed88be6ba80cd75
Lucas Cunha <lucasgalvao260@gmail.com>
2024-05-20 20:25:49 -0300

    Adding exercise 1

---
+++ exercise1.ts
@@ -0,0 +1,14 @@
+let array : number[]  = [2, -2, 3, -3, 4]
+
+if (array.length > 5) {
+    throw new Error('only 5 numbers')
+}
+
+for (let i : number = 1 ; i < array.length ; i++) {
+    if ( array[i]  < 0 ) {
+        array[i] === -1
+    } else {
+        array[i] === 1
+    }
+}
+console.log(array)
